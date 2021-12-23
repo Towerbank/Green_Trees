f23afbfd933e6d02864a8ae0c650c65701b637fb
Towerbank <59486210+Towerbank@users.noreply.github.com>
2021-12-23 18:27:32 +0300

    day 4

--- script/modal.js
+++ script/modal.js
@@ -1,5 +1,7 @@
 const buttonsOrder = document.querySelectorAll('.product__button_order');
 const overlayOrder = document.querySelector('.overlay_order');
+const buttonConsultation = document.querySelector('.header__consultation')
+const overlayConsultation = document.querySelector('.overlay_consultation');
 const order = overlayOrder.querySelector('.modal__order');
 
 buttonsOrder.forEach(buttonOrder => {
@@ -15,5 +17,19 @@ overlayOrder.addEventListener('click', (event) => {
     overlayOrder.classList.remove('overlay_active');
   }
 });
+
+
+
+buttonConsultation.addEventListener('click', () => {
+  overlayConsultation.classList.add('overlay_active');
+})
+
+overlayConsultation.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target === overlayConsultation || target.closest('.modal__close')) {
+    overlayConsultation.classList.remove('overlay_active');
+  }
+})
+
 
 
